ad3523609d986789d6d718d1d10a04a015ccd467
espringer21 <espringer1@nait.ca>
2024-01-22 09:45:20 -0700

    .

--- assessments/a1/lib/main.dart
+++ assessments/a1/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: const Text("App1 - UI Layout"),
+          
           backgroundColor: Colors.blue,
         ),
         body: const Row(
@@ -30,7 +31,37 @@ class MyApp extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             //TODO: Put your code here to complete this app.
+            Column(
+              children: <Widget> [
+                Container1()
+              ],
+
+            ),
+            Column(
+
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Container1 extends StatelessWidget {
+  const Container1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          // decoration: ,
+          height: 100.0,
+          width: 100.0,
+          color: Color.fromARGB(255, 254, 190, 25),
+          child: const Center(
+          child: Text(
+          'Container 1',
         ),
       ),
     );
